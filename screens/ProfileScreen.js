07e5eb6f7ee521db091ea2fa192ad2e939bb9b84
Clerissa <clerissalewis@gmail.com>
2021-03-29 18:49:59 +0400

    Added fields to profile screen

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -17,59 +17,8 @@ import PostCard from '../components/PostCard';
 const ProfileScreen = ({navigation, route}) => {
   const {user, logout} = useContext(AuthContext);
 
-  const [posts, setPosts] = useState([]);
   const [loading, setLoading] = useState(true);
-  const [deleted, setDeleted] = useState(false);
   const [userData, setUserData] = useState(null);
-
-  const fetchPosts = async () => {
-    try {
-      const list = [];
-
-      await firestore()
-        .collection('posts')
-        .where('userId', '==', route.params ? route.params.userId : user.uid)
-        .orderBy('postTime', 'desc')
-        .get()
-        .then((querySnapshot) => {
-          // console.log('Total Posts: ', querySnapshot.size);
-
-          querySnapshot.forEach((doc) => {
-            const {
-              userId,
-              post,
-              postImg,
-              postTime,
-              likes,
-              comments,
-            } = doc.data();
-            list.push({
-              id: doc.id,
-              userId,
-              userName: 'Test Name',
-              userImg:
-                'https://lh5.googleusercontent.com/-b0PKyNuQv5s/AAAAAAAAAAI/AAAAAAAAAAA/AMZuuclxAM4M1SCBGAO7Rp-QP6zgBEUkOQ/s96-c/photo.jpg',
-              postTime: postTime,
-              post,
-              postImg,
-              liked: false,
-              likes,
-              comments,
-            });
-          });
-        });
-
-      setPosts(list);
-
-      if (loading) {
-        setLoading(false);
-      }
-
-      console.log('Posts: ', posts);
-    } catch (e) {
-      console.log(e);
-    }
-  };
 
   const getUser = async() => {
     await firestore()
@@ -86,7 +35,6 @@ const ProfileScreen = ({navigation, route}) => {
 
   useEffect(() => {
     getUser();
-    fetchPosts();
     navigation.addListener("focus", () => setLoading(!loading));
   }, [navigation, loading]);
 
@@ -135,22 +83,11 @@ const ProfileScreen = ({navigation, route}) => {
 
         <View style={styles.userInfoWrapper}>
           <View style={styles.userInfoItem}>
-            <Text style={styles.userInfoTitle}>{posts.length}</Text>
-            <Text style={styles.userInfoSubTitle}>Posts</Text>
-          </View>
-          <View style={styles.userInfoItem}>
-            <Text style={styles.userInfoTitle}>10,000</Text>
-            <Text style={styles.userInfoSubTitle}>Followers</Text>
-          </View>
-          <View style={styles.userInfoItem}>
-            <Text style={styles.userInfoTitle}>100</Text>
-            <Text style={styles.userInfoSubTitle}>Following</Text>
+            <Text style={styles.userInfoTitle}>Email</Text>
+            <Text style={styles.userInfoSubTitle}>{userData.email}</Text>
           </View>
         </View>
 
-        {posts.map((item) => (
-          <PostCard key={item.id} item={item} onDelete={handleDelete} />
-        ))}
       </ScrollView>
     </SafeAreaView>
   );
